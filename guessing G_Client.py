f5ba2d44c3c21fa62617b0a436ac686b568eeb9d
xyrilplayz <xyrilplayz@gmail.com>
2024-05-01 15:47:35 +0800

    G_Client

--- guessing G_Client.py
+++ guessing G_Client.py
@@ -4,19 +4,20 @@ import socket
 host = "192.168.68.105"
 port = 7777
 
-
 s = socket.socket()
 s.connect((host, port))
 
+name = input("Enter your name: ")
+s.sendall(name.encode())
 # received the banner
 data = s.recv(1024)
 # print banner
 print(data.decode().strip())
+print("In What level do you wish to play? \n[1]Easy(1-50) \n[2]Medium(1-100) \n[3]Hard(1-500)")
 
 while True:
     #let get our input from the user
     user_input = input("").strip()
-
     s.sendall(user_input.encode())
     reply = s.recv(1024).decode().strip()
     if "Correct" in reply:
